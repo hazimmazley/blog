651474b96ae12491184b5c960dbb70e0e31f8776
hazimmazley <hazimmazley@gmail.com>
2021-07-11 05:49:53 +0800

    1st commit

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Post;
+use Faker\Generator as Faker;
+
+$factory->define(Post::class, function (Faker $faker) {
+    return [
+        'title' => $faker->text(20),
+        'content' => $faker->text(1000),
+        'category_id' => mt_rand(1, 5)
+    ];
+});
